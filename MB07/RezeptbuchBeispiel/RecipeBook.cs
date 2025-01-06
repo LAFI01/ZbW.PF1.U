65f69d211a0a00c2721b3ea6b83247521d6ed635
Larissa Fitze <laerryies@gmail.com>
2025-01-06 13:45:51 +0100

    Add RecipeBook

---
+++ MB07/RezeptbuchBeispiel/RecipeBook.cs
@@ -0,0 +1,22 @@
+﻿namespace MB07.RezeptbuchBeispiel
+{
+    public partial class RecipeBook : Form
+    {
+
+        public RecipeBook()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CmdAddRecipe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdRecipeDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
