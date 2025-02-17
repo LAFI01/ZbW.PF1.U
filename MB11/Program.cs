074a747d6e4b5d8c90b2c6c60bdfb179d6c8968e
Larissa Fitze <laerryies@gmail.com>
2025-02-17 18:48:55 +0100

    algopro

--- MB11/Program.cs
+++ MB11/Program.cs
@@ -1,12 +1,24 @@
 ﻿using MB11.EinfuehrungsBeispiel;
+using MB11.ZifferSortierAufgabe;
 
 public class Program
 {
     public static void Main()
     {
         // Einführungsbeispiel
-        LogView.PrintReadAllLogs();
-        Console.ReadLine();
+        //LogView.PrintReadAllLogs();
+        //Console.ReadLine();
+
+
+        var input = Console.ReadLine();
+
+        //var 1
+        var z = new ZifferMaschine();
+        var resultat2 = z.AlgoNichtStatic(input);
+        //var 2
+        var resultat = ZifferMaschine.Algo(input);
+
+        Console.WriteLine(resultat);
 
 
 
--- MB11/ZifferSortierAufgabe/ZifferMaschine.cs
+++ MB11/ZifferSortierAufgabe/ZifferMaschine.cs
@@ -1,9 +1,18 @@
 ﻿namespace MB11.ZifferSortierAufgabe
 {
-    public static class ZifferMaschine
+    public  class ZifferMaschine
     {
         //toDo: implementieren Sie hier den Algoritmus für das Problem "Ziffer sortieren"
-      
+       public static string Algo(string input)
+        {
+            return null;
+        }
+
+        public  string AlgoNichtStatic(string input)
+        {
+            return null;
+        }
+
 
 
     }
