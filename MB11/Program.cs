9fb88f0549103007d76f6c6a3bff2601cf55e4bd
Larissa Fitze <laerryies@gmail.com>
2025-02-17 15:27:19 +0100

    Beispiel, um Algo zu programmieren

--- MB11/Program.cs
+++ MB11/Program.cs
@@ -1,17 +1,21 @@
 ﻿using MB11.EinfuehrungsBeispiel;
+using MB11.ZifferSortierAufgabe;
 
 public class Program
 {
     public static void Main()
     {
         // Einführungsbeispiel
-        LogView.PrintReadAllLogs();
+        //LogView.PrintReadAllLogs();
+        //Console.ReadLine();
+
+
+
+        var input = Console.ReadLine();
+        var output = ZifferMaschine.Methode(input);
+        Console.WriteLine(output);
+
         Console.ReadLine();
-
-
-
-
-
     }
 
 
--- MB11/ZifferSortierAufgabe/ZifferMaschine.cs
+++ MB11/ZifferSortierAufgabe/ZifferMaschine.cs
@@ -1,9 +1,15 @@
 ﻿namespace MB11.ZifferSortierAufgabe
 {
-    public static class ZifferMaschine
+    public static  class ZifferMaschine
     {
         //toDo: implementieren Sie hier den Algoritmus für das Problem "Ziffer sortieren"
       
+        public static string Methode(string input)
+        {
+            return nul; 
+        }
+
+
 
 
     }
